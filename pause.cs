3847bc083af23bdacaf7dd9232958fabf87ba182
phancyn✨ <shvalinc@gmail.com>
2024-07-20 21:36:19 +0300

    Update pause.cs

--- pause.cs
+++ pause.cs
@@ -1,14 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class pause : MonoBehaviour
 {
-   public GameObject panel;
-
+    public GameObject panel;
     public void pausegame()
     { 
         Time.timeScale = 0f;
         panel.SetActive(true);       
+    }
+
+
+
+    public void continuegame()
+    { 
+        Time.timeScale = 1f;
+        panel.SetActive(false);       
     }
 }
