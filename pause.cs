6d28fd304cf3d4d162c2b42d2c1c28eb1cb29feb
phancyn✨ <shvalinc@gmail.com>
2024-08-01 18:54:35 +0300

    Update pause.cs

--- pause.cs
+++ pause.cs
@@ -5,10 +5,21 @@ using UnityEngine;
 public class pause : MonoBehaviour
 {
    public GameObject panel;
-
+    //for button pause
+    if (Input.GetKeyDown(KeyCode.//YoureKey))
+    {
+      pausegame();
+    }
+    
     public void pausegame()
     { 
         Time.timeScale = 0f;
         panel.SetActive(true);       
+    }
+    //To exit menu/pause
+     public void playtogame()
+    { 
+        Time.timeScale = 1f;
+        panel.SetActive(false);       
     }
 }
